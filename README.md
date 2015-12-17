bb369bb99e74c11d2c9905837296d4d14b48cdec
Juan Cruz Viotti <jviottidc@gmail.com>
2015-12-17 13:00:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,119 @@
+elevator
+========
+
+[![npm version](https://badge.fury.io/js/elevator.svg)](http://badge.fury.io/js/elevator)
+[![dependencies](https://david-dm.org/resin-io/elevator.png)](https://david-dm.org/resin-io/elevator.png)
+[![Build status](https://ci.appveyor.com/api/projects/status/ysweh6h4ed4ak114/branch/master?svg=true)](https://ci.appveyor.com/project/resin-io/elevator/branch/master)
+
+Windows UAC elevation that just works.
+
+Description
+-----------
+
+This module is a thin wrapper around the awesome [elevate.exe utility by kliu](http://code.kliu.org/misc/elevate/), which is a small C open source application with x64 and ia32 support that just works.
+
+This module doesn't make use of NodeJS C/C++ bindings and instead relies on executing the `.exe` file direcly in a way that is [electron](http://electron.atom.io) friendly even when the application is packaged in an `asar` archive.
+
+Of course, this means that this NodeJS module is subjected to the Windows versions and architectures such supports. Windows Vista (or newer) is required.
+
+Installation
+------------
+
+Install `elevator` by running:
+
+```sh
+$ npm install --save elevator
+```
+
+Documentation
+-------------
+
+
+* [elevator](#module_elevator)
+    * [.execute(command, [options], callback)](#module_elevator.execute)
+    * [.executeSync(command, [options])](#module_elevator.executeSync) ⇒ <code>String</code>
+
+<a name="module_elevator.execute"></a>
+### elevator.execute(command, [options], callback)
+**Kind**: static method of <code>[elevator](#module_elevator)</code>  
+**Summary**: Execute a command with UAC elevation  
+**Access:** public  
+
+| Param | Type | Default | Description |
+| --- | --- | --- | --- |
+| command | <code>Array.&lt;String&gt;</code> |  | command |
+| [options] | <code>Object</code> | <code>{}</code> | options |
+| [options.terminating] | <code>Boolean</code> |  | Launches a terminating command processor; equivalent to "cmd /c command". |
+| [options.persistent] | <code>Boolean</code> |  | Launches a persistent command processor; equivalent to "cmd /k command". |
+| [options.pushdCurrentDirectory] | <code>Boolean</code> |  | When using -c or -k, do not pushd the current directory before execution. |
+| [options.unicode] | <code>Boolean</code> |  | When using -c or -k, use Unicode; equivalent to "cmd /u". |
+| [options.waitForTermination] | <code>Boolean</code> |  | Waits for termination; equivalent to "start /wait command". |
+| callback | <code>function</code> |  | callback (error, stdout, stderr) |
+
+**Example**  
+```js
+elevator.execute([ 'cmd.exe' ], {
+  waitForTermination: true
+}, function(error, stdout, stderr) {
+  if (error) {
+    throw error;
+  }
+
+  console.log(stdout);
+  console.log(stderr);
+});
+```
+<a name="module_elevator.executeSync"></a>
+### elevator.executeSync(command, [options]) ⇒ <code>String</code>
+**Kind**: static method of <code>[elevator](#module_elevator)</code>  
+**Summary**: Execute a command with UAC elevation (Sync)  
+**Returns**: <code>String</code> - stdout buffer  
+**Access:** public  
+
+| Param | Type | Default | Description |
+| --- | --- | --- | --- |
+| command | <code>Array.&lt;String&gt;</code> |  | command |
+| [options] | <code>Object</code> | <code>{}</code> | options |
+| [options.terminating] | <code>Boolean</code> |  | Launches a terminating command processor; equivalent to "cmd /c command". |
+| [options.persistent] | <code>Boolean</code> |  | Launches a persistent command processor; equivalent to "cmd /k command". |
+| [options.pushdCurrentDirectory] | <code>Boolean</code> |  | When using -c or -k, do not pushd the current directory before execution. |
+| [options.unicode] | <code>Boolean</code> |  | When using -c or -k, use Unicode; equivalent to "cmd /u". |
+| [options.waitForTermination] | <code>Boolean</code> |  | Waits for termination; equivalent to "start /wait command". |
+
+**Example**  
+```js
+elevator.executeSync([ 'cmd.exe' ], {
+  waitForTermination: true
+});
+```
+
+Support
+-------
+
+If you're having any problem, please [raise an issue](https://github.com/resin-io/elevator/issues/new) on GitHub and the Resin.io team will be happy to help.
+
+Tests
+-----
+
+Run the test suite by doing:
+
+```sh
+$ gulp test
+```
+
+Contribute
+----------
+
+- Issue Tracker: [github.com/resin-io/elevator/issues](https://github.com/resin-io/elevator/issues)
+- Source Code: [github.com/resin-io/elevator](https://github.com/resin-io/elevator)
+
+Before submitting a PR, please make sure that you include tests, and that [jshint](http://jshint.com) runs without any warning:
+
+```sh
+$ gulp lint
+```
+
+License
+-------
+
+The project is licensed under the MIT license.
---
+++ tests/command.spec.js
@@ -0,0 +1,256 @@
+var m = require('mochainon');
+var os = require('os');
+var fs = require('fs');
+var path = require('path');
+var command = require('../lib/command');
+
+describe('Command', function() {
+
+  describe('.getBinaryPath()', function() {
+
+    var isAbsolute = function(x) {
+      return path.resolve(x) === x;
+    };
+
+    describe('given x86', function() {
+
+      beforeEach(function() {
+        this.osArchStub = m.sinon.stub(os, 'arch');
+        this.osArchStub.returns('x64');
+      });
+
+      afterEach(function() {
+        this.osArchStub.restore();
+      });
+
+      it('should return the path to the x64 executable', function() {
+        var binPath = command.getBinaryPath();
+        var expected = path.join(__dirname, '..', 'bin', 'elevate-x64.exe');
+        m.chai.expect(binPath).to.equal(expected);
+      });
+
+      it('should return an absolute path', function() {
+        var binPath = command.getBinaryPath();
+        m.chai.expect(isAbsolute(binPath)).to.be.true;
+      });
+
+      it('should exist', function() {
+        m.chai.expect(function() {
+          fs.statSync(command.getBinaryPath());
+        }).to.not.throw();
+      });
+
+    });
+
+    describe('given ia32', function() {
+
+      beforeEach(function() {
+        this.osArchStub = m.sinon.stub(os, 'arch');
+        this.osArchStub.returns('ia32');
+      });
+
+      afterEach(function() {
+        this.osArchStub.restore();
+      });
+
+      it('should return the path to the ia32 executable', function() {
+        var binPath = command.getBinaryPath();
+        var expected = path.join(__dirname, '..', 'bin', 'elevate-ia32.exe');
+        m.chai.expect(binPath).to.equal(expected);
+      });
+
+      it('should return an absolute path', function() {
+        var binPath = command.getBinaryPath();
+        m.chai.expect(isAbsolute(binPath)).to.be.true;
+      });
+
+      it('should exist', function() {
+        m.chai.expect(function() {
+          fs.statSync(command.getBinaryPath());
+        }).to.not.throw();
+      });
+
+    });
+
+    describe('given arm', function() {
+
+      beforeEach(function() {
+        this.osArchStub = m.sinon.stub(os, 'arch');
+        this.osArchStub.returns('arm');
+      });
+
+      afterEach(function() {
+        this.osArchStub.restore();
+      });
+
+      it('should throw a non supported error', function() {
+        m.chai.expect(function() {
+          command.getBinaryPath();
+        }).to.throw('Unsupported arch: arm');
+      });
+
+    });
+
+  });
+
+  describe('.build()', function() {
+
+    describe('given no command', function() {
+
+      it('should throw an error', function() {
+        m.chai.expect(function() {
+          command.build(null);
+        }).to.throw('Missing command');
+      });
+
+    });
+
+    describe('given no options', function() {
+
+      it('should return the correct command', function() {
+        var result = command.build([ 'foo' ]);
+        m.chai.expect(result).to.deep.equal([ 'foo' ]);
+      });
+
+    });
+
+    describe('given terminating = true', function() {
+
+      it('should return the correct command', function() {
+        var result = command.build([ 'foo' ], {
+          terminating: true
+        });
+
+        m.chai.expect(result).to.deep.equal([ '-c', 'foo' ]);
+      });
+
+    });
+
+    describe('given terminating = true and a multiple word command', function() {
+
+      it('should return the correct command', function() {
+        var result = command.build([ 'foo', 'bar' ], {
+          terminating: true
+        });
+
+        m.chai.expect(result).to.deep.equal([ '-c', 'foo', 'bar' ]);
+      });
+
+    });
+
+    describe('given persistent = true', function() {
+
+      it('should return the correct command', function() {
+        var result = command.build([ 'foo' ], {
+          persistent: true
+        });
+
+        m.chai.expect(result).to.deep.equal([ '-k', 'foo' ]);
+      });
+
+    });
+
+    describe('given persistent = true and terminating = true', function() {
+
+      it('should throw an error', function() {
+        m.chai.expect(function() {
+          command.build([ 'foo' ], {
+            terminating: true,
+            persistent: true
+          });
+        }).to.throw('Can\'t have a both persistent and terminating command processor');
+      });
+
+    });
+
+    describe('given terminating = true and pushdCurrentDirectory = true', function() {
+
+      it('should return the correct command', function() {
+        var result = command.build([ 'foo' ], {
+          terminating: true,
+          pushdCurrentDirectory: true
+        });
+
+        m.chai.expect(result).to.deep.equal([ '-c', '-n', 'foo' ]);
+      });
+
+    });
+
+    describe('given persistent = true and pushdCurrentDirectory = true', function() {
+
+      it('should return the correct command', function() {
+        var result = command.build([ 'foo' ], {
+          persistent: true,
+          pushdCurrentDirectory: true
+        });
+
+        m.chai.expect(result).to.deep.equal([ '-k', '-n', 'foo' ]);
+      });
+
+    });
+
+    describe('given only pushdCurrentDirectory = true', function() {
+
+      it('should throw an error', function() {
+        m.chai.expect(function() {
+          command.build([ 'foo' ], {
+            pushdCurrentDirectory: true
+          });
+        }).to.throw('pushdCurrentDirectory requires the terminating or persistent option');
+      });
+
+    });
+
+    describe('given terminating = true and unicode = true', function() {
+
+      it('should return the correct command', function() {
+        var result = command.build([ 'foo' ], {
+          terminating: true,
+          unicode: true
+        });
+
+        m.chai.expect(result).to.deep.equal([ '-c', '-u', 'foo' ]);
+      });
+
+    });
+
+    describe('given persistent = true and unicode = true', function() {
+
+      it('should return the correct command', function() {
+        var result = command.build([ 'foo' ], {
+          persistent: true,
+          unicode: true
+        });
+
+        m.chai.expect(result).to.deep.equal([ '-k', '-u', 'foo' ]);
+      });
+
+    });
+
+    describe('given only unicode = true', function() {
+
+      it('should throw an error', function() {
+        m.chai.expect(function() {
+          command.build([ 'foo' ], {
+            unicode: true
+          });
+        }).to.throw('unicode requires the terminating or persistent option');
+      });
+
+    });
+
+    describe('given waitForTermination = true', function() {
+
+      it('should return the correct command', function() {
+        var result = command.build([ 'foo' ], {
+          waitForTermination: true
+        });
+
+        m.chai.expect(result).to.deep.equal([ '-w', 'foo' ]);
+      });
+
+    });
+
+  });
+
+});
